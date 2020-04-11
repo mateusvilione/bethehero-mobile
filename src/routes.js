2389341000203c52211df330b56a06946133dbff
Mateus Vilione <mateusvilione@gmail.com>
2020-04-11 20:05:41 -0300

    Estilo da page incidents

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -8,8 +9,8 @@ import Detail from './pages/Detail';
 
 export default function Routes() {
     return(
-        <NavigationContainer>
-            <AppStack.Navigator>
+        <NavigationContainer >
+            <AppStack.Navigator screenOptions={{ headerShown: false }}>
                 <AppStack.Screen name="Incidents" component={Incidents} />
                 <AppStack.Screen name="Detail" component={Detail} />
             </AppStack.Navigator>
