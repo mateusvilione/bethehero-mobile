8b701e5372ded9684fb1b19b4162533586c48faa
Mateus Vilione <mateusvilione@gmail.com>
2020-04-12 19:33:40 -0300

    mobile

--- src/pages/Detail/styles.js
+++ src/pages/Detail/styles.js
@@ -12,53 +12,57 @@ export default StyleSheet.create({
         justifyContent: 'space-between',
         alignItems: 'center',
     },
-    headerText: {
-        fontSize: 15,
-        color: '#737380',
-    },
-    headerTextBold: {
-        fontWeight: 'bold',
-    },
-    title: {
-        fontSize: 30,
-        marginBottom: 16,
-        marginTop: 48,
-        color: '#13131a',
-        fontWeight: 'bold',
-    },
-    description: {
-        fontSize: 16,
-        lineHeight: 24,
-        color: '#737380',
-    },
-    incidentsList: {
-        marginTop: 32,
-    },
     incident: {
         padding: 24,
         borderRadius: 8,
         backgroundColor: '#FFF',
         marginBottom: 16,
+        marginTop: 48,
     },
     incidentProperty: {
         fontSize: 14,
         color: '#41414d',
         fontWeight: 'bold',
+        marginTop: 24,
     },
     incidentValue: {
         marginTop: 8,
         fontSize: 15,
-        marginBottom: 24,
         color: '#737380',
     },
-    detailsButton: {
+    contactBox: {
+        padding: 24,
+        borderRadius: 8,
+        backgroundColor: '#FFF',
+        marginBottom: 16,
+    },
+    heroTitke: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        color: '#13131a',
+        lineHeight: 30,
+    },
+    heroDescription: {
+        fontSize: 15,
+        color: '#737380',
+        marginTop: 16,
+    },
+    actions: {
+        marginTop: 16,
         flexDirection: 'row',
         justifyContent: 'space-between',
+    },
+    action: {
+        backgroundColor: '#E02041',
+        borderRadius: 8,
+        height: 50,
+        width: '48%',
+        justifyContent: 'center',
         alignItems: 'center',
     },
-    detailsButtonText: {
-        color: '#E02041',
+    actionText: {
+        color: '#FFF',
         fontSize: 15,
         fontWeight: 'bold',
-    },    
+    },
 })
