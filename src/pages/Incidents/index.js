2d5a6451d928a640fb27e67ff305671ccfd45130
Mateus Vilione <mateusvilione@gmail.com>
2020-05-11 16:33:02 -0300

    correção de campos

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -8,55 +8,59 @@ import styles from './styles';
 import api from '../../services/api';
 
 export default function Incidents() {
-    const [incidents, setIncidents] = useState([]);
-    const [total, setTotal] = useState(0);
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(false);
+    const [incidents, setIncidents] = useState([])
+    const [totalIncidents, setTotalIncidents] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [loading, setLoading] = useState(false)
 
-    const navigation = useNavigation();
+    const navigation = useNavigation()
 
-    function navigateToDetail(incident) {
-        navigation.navigate('Detail', { incident });
+    function navigateToDetails(incident) {
+        navigation.navigate('Detail', { incident })
     }
 
     async function loadIncidents() {
         if (loading) {
-            return;
+            return
         }
 
-        if (total > 0 && incidents.length == total) {
-            return;
+        if (totalIncidents > 0 && incidents.length === totalIncidents) {
+            return
         }
 
-        setLoading(true);
+        setLoading(true)
 
-        const response = await api.get('incidents', {
-            params: { page }
-        });
+        const response = await api.get('/incidents', {
+            params: {
+                page: currentPage
+            }
+        })
 
-        setIncidents([...incidents, ...response.data]);
-        setTotal(response.headers['x-total-count']);
-        setPage(page + 1);
-        setLoading(false);
+        setIncidents([...incidents, ...response.data])
+        setTotalIncidents(response.headers['X-Total-Count'])
+
+        setCurrentPage(currentPage + 1)
+        setLoading(false)
     }
 
     useEffect(() => {
-        loadIncidents();
-    }, []);
+        loadIncidents()
+    }, [])
 
     return (
         <View style={styles.container}>
+
             <View style={styles.header}>
                 <Image source={logoImg} />
                 <Text style={styles.headerText}>
-                    Total de <Text style={styles.headerTextBold}>{total} casos</Text>
+                    Total de <Text style={styles.headerTextBold}>{totalIncidents}</Text> casos
                 </Text>
             </View>
-            <Text style={styles.title}>Bem-vindo!</Text>
-            <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia!</Text>
+            <Text style={styles.mainTitle}>Bem-vindo</Text>
+            <Text style={styles.mainDescription}>Escolha um dos casos abaixo e salve o dia!</Text>
 
             <FlatList
-                data={[incidents]}
+                data={incidents}
                 style={styles.incidentsList}
                 keyExtractor={incident => String(incident.id)}
                 showsVerticalScrollIndicator={false}
@@ -68,14 +72,14 @@ export default function Incidents() {
                         <Text style={styles.incidentProperty}>ONG:</Text>
                         <Text style={styles.incidentValue}>{incident.name}</Text>
 
-                        <Text style={styles.incidentProperty}>CASO::</Text>
-                        <Text style={styles.incidentValue}>{incident.title}</Text>
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>{incident.description}</Text>
 
                         <Text style={styles.incidentProperty}>VALOR:</Text>
                         <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</Text>
 
                         <TouchableOpacity style={styles.detailsButton}
-                            onPress={() => navigateToDetail(incident)}
+                            onPress={() => navigateToDetails(incident)}
                         >
                             <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
                             <Feather name="arrow-right" size={16} color="#E02041" />
